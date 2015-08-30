938fd940930e462d49712b2cb822f5e25487ae48
GrantSchoffelen <grant.schoffelen@gmail.com>
2015-08-30 12:52:36 -0400

    new spinner

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -50,15 +50,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
+  // .state('tab.chats', {
+  //     url: '/chats',
+  //     views: {
+  //       'tab-chats': {
+  //         templateUrl: 'templates/tab-chats.html',
+  //         controller: 'ChatsCtrl'
+  //       }
+  //     }
+  //   })
     .state('tab.chat-detail', {
       url: '/chats/:chatId',
       views: {
